dddeb9145f4966df893fcf9fb496444bda0dabc4
aiisiik <ai.sen@mail.ru>
2023-03-26 16:11:27 +0900

    Добавьте файлы проекта.

---
+++ SB19.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+	virtual void Voice() {
+		std::cout << "I am an animal" << endl;
+	}
+};
+
+class Dog : public Animal {
+public:
+	void Voice() override {
+		std::cout << "Woof!" << endl;
+	}
+};
+
+class Cat : public Animal {
+public:
+	void Voice() override {
+		std::cout << "Meow!" << endl;
+	}
+};
+
+class Duck : public Animal {
+public:
+	void Voice() override {
+		std::cout << "Quack!" << endl;
+	}
+};
+
+class Horse : public Animal {
+public:
+	void Voice() override {
+		std::cout << "Neigh!" << endl;
+	}
+};
+
+int main()
+{
+	const int AnimalsCount = 4;
+	Animal* animals[AnimalsCount];
+	animals[0] = new Dog();
+	animals[1] = new Cat();
+	animals[2] = new Duck();
+	animals[3] = new Horse();
+	for (int i = 0; i < AnimalsCount; i++) {
+		animals[i]->Voice();
+	}
+}
---
+++ SB19_alt.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+#include <string>
+#include <random>
+
+const int NamesCount = 15;
+const std::string Names[NamesCount] =
+{ "Bella","Max","Luna","Charlie","Lucy","Cooper","Daisy","Milo","Zoe","Rocky","Hana","Yumi","Pikachu","Yuki","Nami"};
+const int VoicesCount = 18;
+const std::string AnimalType[VoicesCount] =
+{ "cat","dog","horse","goat","pig","cow","donkey","chicken","rooster",
+"bird","owl","duck","goose","gobble","mosquitoe","cricket","frog","lion" };
+const std::string Voices[VoicesCount] =
+{ "meow","woof","neigh","baa","oink","moo","hee-haw","cluck","cock-a-doodle-do",
+"chirp","Hoot","quack","honk","gobble","buzz","chirp","ribbit","roar" };
+
+
+
+class Animal{
+public:
+	Animal(std::string _Name = "Animal", std::string _VoiceText = "...") : Name(_Name), VoiceText(_VoiceText) {
+		Name = Names[rand()% NamesCount];
+		VoiceText = Voices[rand() % VoicesCount];
+	}
+	virtual void Voice(){
+		std::cout << Name <<" said: " << VoiceText << std::endl;
+	};
+	std::string GetName() {
+		return Name;
+	}
+private:
+	std::string VoiceText;
+	std::string Name;
+};
+
+class Dog : Animal{
+public:
+	Dog (){}
+	void Voice() override{
+		std::cout << GetName() << " said: " "Woof!" << std::endl;
+	};
+};
+
+
+class Cat : Animal{
+public:
+	Cat() {}
+	void Voice() override {
+		std::cout << GetName() << " said: " "Meow!" << std::endl;
+	};
+};
+
+int main(){
+	Animal* arr[3];
+	arr[0] = new Dog();
+	arr[1] = new Cat();
+	arr[2] = new Cow();
+
+	for (int i = 0; i < 3; i++) {
+		arr[i]->Voice();
+	}
+
+	const int AnimalsCount = 10;
+	Animal* animals[AnimalsCount];
+
+
+	for (int i = 0; i < AnimalsCount; i++) {
+		switch (rand() % 3)
+		{
+		case 0: animals[i] = new Dog();
+		default: animals[i] = new Animal();
+		}
+	}
+
+
+}
